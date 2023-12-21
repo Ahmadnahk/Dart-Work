8703e1fa3df8db55f1cfddf6e680bc4eaa799541
atif <atif@codenterprise.com>
2023-12-21 22:33:28 +0500

    project has been initlized

---
+++ variable_datatype/bin/variable_datatype.dart
@@ -0,0 +1,5 @@
+import 'package:variable_datatype/variable_datatype.dart' as variable_datatype;
+
+void main(List<String> arguments) {
+  print('Hello world: ${variable_datatype.calculate()}!');
+}
---
+++ variable_datatype/test/variable_datatype_test.dart
@@ -0,0 +1,8 @@
+import 'package:variable_datatype/variable_datatype.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
